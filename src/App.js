fdb6f781e0cb380efcb137923dc026dd457885e3
yhkim <yhkim@hunesion.com>
2020-09-12 16:40:57 +0900

    클론코딩03 완료 후 거밋

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+function Hello() {
+  return <h3>Hello</h3>;
+}
+
+function Food({ fav }) {
+  return <h1>I like {fav}</h1>;
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Hello />
+      <Food fav="kimchi" />
+      <Food fav="라면" />
+      <Food fav="볶음밥" />
+      <Food fav="소고기" />
     </div>
   );
 }
