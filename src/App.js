af07062a78fffc0ad3e5fb9a39c0025f9d29f3a0
yhkim <yhkim@hunesion.com>
2020-09-12 20:39:21 +0900

    클론코딩05 component class lifecycle function 처리

--- src/App.js
+++ src/App.js
@@ -1,78 +1,46 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-//
-const foodILike = [
-  {
-    id: 1,
-    name: '김치',
-    image:
-      'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fgdimg.gmarket.co.kr%2F911872451%2Fstill%2F600%3Fver%3D1509588369&f=1&nofb=1',
-    rating: 5,
-  },
-  {
-    id: 2,
-    name: '삼겹살',
-    image:
-      'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.v0RiALj-qb0tA3TaocYnVAHaE7%26pid%3DApi%26h%3D160&f=1',
-    rating: 3.5,
-  },
-  {
-    id: 3,
-    name: '감자전',
-    image:
-      'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.90_RHvq4OhfpzkVXnILfrQHaEf%26pid%3DApi%26h%3D160&f=1',
-    rating: 5.5,
-  },
-  {
-    id: 4,
-    name: '한우',
-    image:
-      'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.Ecec8cp64FDhy5rw4TxI_AHaHn%26pid%3DApi%26h%3D160&f=1',
-    rating: 4.5,
-  },
-];
 
-Food.propTypes = {
-  name: PropTypes.string.isRequired,
-  picture: PropTypes.string.isRequired,
-  rating: PropTypes.number.isRequired,
-};
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log('constructor');
+  }
 
-function Food({ name, picture, rating }) {
-  return (
-    <div>
-      <h1>I like {name}</h1>
-      <h4>{rating}/5.0</h4>
-      <img src={picture} alt={name} />
-    </div>
-  );
-}
+  componentDidMount() {
+    console.log('component rendered');
+  }
 
-// function renderFood(dish) {
-//   return <Food name={dish.name} picture={dish.image} />;
-// }
+  componentDidUpdate() {
+    console.log('component updated');
+  }
 
-// const renderFood = function (dish) {
-//   return <Food name={dish.name} picture={dish.image} />;
-// };
+  componentWillUnmount() {
+    console.log('component unmounted');
+  }
 
-// const renderFood = (dish) => (
-//   <Food key={dish.id} name={dish.name} image={dish.image} />
-// );
+  state = {
+    count: 0,
+  };
 
-function App() {
-  return (
-    <div>
-      {foodILike.map((dish) => (
-        <Food
-          key={dish.id}
-          name={dish.name}
-          picture={dish.image}
-          rating={dish.rating}
-        />
-      ))}
-    </div>
-  );
+  add = () => {
+    console.log('add');
+    this.setState((current) => ({ count: current.count + 1 }));
+  };
+  minus = () => {
+    console.log('minus');
+    this.setState((currnt) => ({ count: currnt.count - 1 }));
+  };
+
+  render() {
+    console.log('I am rendering');
+    return (
+      <div>
+        <h1>The number is {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    );
+  }
 }
 
 export default App;
