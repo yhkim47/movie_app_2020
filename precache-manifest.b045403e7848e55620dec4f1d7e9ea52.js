8f77211a472b95cfd4ba0cadd96584792c143b68
yhkim <yhkim@hunesion.com>
2020-09-13 00:40:39 +0900

    Updates

---
+++ precache-manifest.b045403e7848e55620dec4f1d7e9ea52.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90f8a483052a5bca816889a98e3291e4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "322a4893ccbad60f3b7a",
+    "url": "/static/css/main.06ff1b0b.chunk.css"
+  },
+  {
+    "revision": "dcbc1ad3746847d335f9",
+    "url": "/static/js/2.968ebb96.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.968ebb96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "322a4893ccbad60f3b7a",
+    "url": "/static/js/main.0c52851b.chunk.js"
+  },
+  {
+    "revision": "bd7f4f55b55e38cea2f1",
+    "url": "/static/js/runtime-main.58002af7.js"
+  }
+]);
